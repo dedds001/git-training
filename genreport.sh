589f0fbcebca39dfa26afba0d9bcc290ea9812dc
EC2 Default User <ec2-user@ip-172-31-84-233.ec2.internal>
2019-06-26 19:14:58 +0000

    my ip address and hostname

--- genreport.sh
+++ genreport.sh
@@ -1,1 +1,18 @@
-ifconfig
+eth0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 9001
+        inet 172.31.84.233  netmask 255.255.240.0  broadcast 172.31.95.255
+        inet6 fe80::103b:9cff:feab:e818  prefixlen 64  scopeid 0x20<link>
+        ether 12:3b:9c:ab:e8:18  txqueuelen 1000  (Ethernet)
+        RX packets 44310  bytes 42461356 (40.4 MiB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 11128  bytes 1329331 (1.2 MiB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
+        inet 127.0.0.1  netmask 255.0.0.0
+        inet6 ::1  prefixlen 128  scopeid 0x10<host>
+        loop  txqueuelen 1000  (Local Loopback)
+        RX packets 8  bytes 648 (648.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 8  bytes 648 (648.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
